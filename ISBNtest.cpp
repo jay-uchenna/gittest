807e1107651d1914c31b32ad2c6c548078b20232
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-05-13 19:42:49 +0300

    added 2 funtions

--- ISBNtest.cpp
+++ ISBNtest.cpp
@@ -4,13 +4,37 @@
 void error_message(){
 	std::cout << "ISBN not entered correctly\n";
 }
+bool check_ISBN_has_only_int(std::string ISBN){
+	int i = 0;
+	while (i < 13){
+		if (ISBN[i] < 48 || ISBN[i] > 57){
+			return false;
+		}
+		i++;
+	}
+	return true;
+}
 
 
 
 int main(){
 
 	std::string ISBN;
-	getline(cin, ISBN);
+	getline(std::cin, ISBN);
+
+	if (ISBN.length() != 13){
+		error_message();
+	}
+	else {
+		//std::cout << "here 1\n";
+		if (check_ISBN_has_only_int(ISBN)){
+
+		}
+		else {
+			error_message();
+		}
+
+	}
 
 	return 0;
 }

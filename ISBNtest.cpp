73f66347db196178358b256eef4adbfd73c47ab1
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-05-14 12:49:16 +0300

    good practice

--- ISBNtest.cpp
+++ ISBNtest.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
 #include <string>
 
-void error_message(){
+void Error_Message(){
 	std::cout << "ISBN not entered correctly\n";
 }
-bool check_ISBN_has_only_int(std::string ISBN){
-	int i = 0;
-	while (i < 13){
+bool Check_ISBN_has_only_int(std::string ISBN){
+    for(int i = 0; i < 13; i++){
 		if (ISBN[i] < '0' || ISBN[i] > '9'){
 			return false;
 		}
-		i++;
 	}
 	return true;
 }
-bool check_ISBN_is_valid(std::string ISBN){
+bool Check_ISBN_is_valid(std::string ISBN){
 	int sum = 0, i = 0;
-	while (i < 12){
+	for ( ; i < 12; ){
 		sum += (((int)ISBN[i++] - '0') * 1);
 		sum += (((int)ISBN[i++] - '0') * 3);
 	}
-	if (((sum%10) == 0) && (((int)ISBN[i] - '0') == 0)){
+	if ((((sum%10) == 0) && (((int)ISBN[i] - '0') == 0))||
+			((10 -(sum%10)) ==  ((int)ISBN[i] - '0'))){
 	 	return true;
 	}
-	else if ((10 -(sum%10)) ==  ((int)ISBN[i] - '0')) {
-		return true;
-	}
-	else {
-		return false;
-	}
-
+	return false;
 }
 
 int main(){
@@ -38,21 +31,13 @@ int main(){
 	std::string ISBN;
 	getline(std::cin, ISBN);
 
-	if (ISBN.length() != 13){
-		error_message();
+	if ((ISBN.length() != 13)||
+		(!Check_ISBN_has_only_int(ISBN))||
+			(!Check_ISBN_is_valid(ISBN))){
+		Error_Message();
 	}
 	else {
-		if (!check_ISBN_has_only_int(ISBN)){
-			error_message();
-		}
-		else {
-			if (!check_ISBN_is_valid(ISBN)) {
-				error_message();
-			}
-			else std::cout << ISBN << " is a valid ISBN\n";
-		}
-
+		std::cout << ISBN << " is a valid ISBN\n";
 	}
-
 	return 0;
 }
